8f6baa1494d1af93673aecd07e6496a1773ea28b
rjaniak <rajkonkret660@gmail.com>
2024-12-06 15:43:42 +0100

    [3.12] add wykres.py

---
+++ day_3/wykres.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# pip install matplotlib
+
+x = [1, 2, 3, 4, 5]
+y = [2, 5, 7, 9, 11]
+
+plt.plot(x, y, color="red")
+plt.title("Wykres liniowy")
+plt.xlabel("Oś X")
+plt.ylabel("Oś y")
+
+plt.savefig('wykres.png')
+plt.show()
